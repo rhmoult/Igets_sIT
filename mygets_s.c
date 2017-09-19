fcf7e2c81d65dc063f094f43dd49e149b6092b1c
Rich Moulton <rhmoult@gmail.com>
2017-09-19 16:56:03 -0400

    Update mygets_s.c
    Changed gets_s to ensure we start with a NULL pointer in buffer

--- mygets_s.c
+++ mygets_s.c
@@ -3,11 +3,15 @@
 
 // C11 Annex K does not appear to be supported on my compiler
 // Therefore, I define my own gets_s
-void * gets_s(char *buffer, size_t sizeInCharacters) {
-
-// fgets will read in sizeInCharacters - 1 or up until EOF or '\n'
-// It will also append a NULL terminator after the characters copied in
+void * gets_s(char *buffer, int sizeInCharacters) {
+    
+    buffer = NULL;
+    
+    // fgets will read in sizeInCharacters - 1 or up until EOF or '\n'
+    // It will also append a NULL terminator after the characters copied in
     fgets(buffer, sizeInCharacters, stdin);
+    
+    // If successful...
     if (buffer) {
         for (int i = 0; i < (sizeInCharacters - 1); i++) {
             // If there is a newline, we have good, nonoverflowed content
@@ -16,18 +20,21 @@ void * gets_s(char *buffer, size_t sizeInCharacters) {
             // If there was no newline, someone was trying to overflow the buffer
         }
     }
-
-    // We'll just clear the input buffer in the 'else' case
-    int c;
-    while ((c = getchar()) != '\n' && c!= EOF);
-    return (void *)0;
-
+    else {
+        // We'll just clear the input buffer in the 'else' case
+        int c;
+        while ((c = getchar()) != '\n' && c!= EOF);
+    }
+    
+    // Either we have no buffer or there is no newline in the buffer
+    return NULL;
+    
 };
 
 //  This is a test function for gets_s
 void get_y_or_n(void) {
     char response[8];
-    size_t len = sizeof(response);
+    int len = (int)sizeof(response);
     puts("Continue? [y] n: ");
     gets_s(response, len);
     if(response[0] == 'n')
@@ -35,9 +42,14 @@ void get_y_or_n(void) {
 }
 
 // Here, I will run the test functions until the user quits from within the function
-int main() {
+int main(int argc, const char * argv[]) {
+
+    // insert code here...
+    printf("Hello, World!\n");
+    
     while(1) {
-       get_y_or_n();
+        get_y_or_n();
     }
+
     return 0;
 }
